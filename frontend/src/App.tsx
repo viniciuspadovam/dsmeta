b3f9a0da434f8ee570955cf5db9806861e477bd5
viniciuspadovam <padovam3373@gmail.com>
2022-07-13 00:27:29 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import { Header } from "./components/Header";
+import { SalesCard } from "./components/SalesCard";
 import { NotificationButton } from "./components/NotificationButton";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
 
-export default App
+export default App;
