3bdf77ae9bda5f7de04fdc6b73c8555d8bf49049
viniciuspadovam <padovam3373@gmail.com>
2022-07-13 11:57:20 -0300

    Date Select

--- backend/src/main/java/com/viniciuspadovam/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/viniciuspadovam/dsmeta/services/SalesService.java
@@ -1,8 +1,12 @@
 package com.viniciuspadovam.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.viniciuspadovam.dsmeta.entities.Sales;
@@ -13,7 +17,12 @@ public class SalesService {
 	@Autowired
 	private SalesRepository repository;
 	
-	public List<Sales> findSales() {
-		return repository.findAll();
+	public Page<Sales> findSales(String minDate, String maxDate, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 }
